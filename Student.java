f961debdf2c1a7dd4d39d14c0317f49feeab7cbf
JasonDanielLyle <39168382+JasonDanielLyle@users.noreply.github.com>
2018-06-07 12:03:44 -0700

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,46 @@
+
+public class Student {
+    
+    //datafields
+    private String name;     // student name
+    private String email;    // email 
+  
+    
+    
+    
+    //constructors
+    public Student(){} 
+    
+    public Student(String name, String email) {
+        this.name   = name;
+        this.email   = email;
+        }
+    
+    
+    //getter and setter methods
+    public String getName(){
+	return this.name;
+	}
+    public void setName(String a){
+	this.name = a;
+	}
+	
+    
+    public String getEmail(){
+	return this.email;
+    } 
+    public void setEmail(String b){
+	this.email = b;
+    }
+    
+   
+   
+ 
+
+}
+
+
+    
+    
+    
+   
